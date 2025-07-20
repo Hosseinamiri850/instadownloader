663d86a36a2abc7fc09b1c76f77c65d85f1f667a
Hosseinamiri850 <144267766+Hosseinamiri850@users.noreply.github.com>
2025-07-20 21:30:50 +0330

    Update instadownloader.py

--- instadownloader.py
+++ instadownloader.py
@@ -140,7 +140,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.edit_message_text(f"❌ خطا:\n{str(e)}")
 
 # مقداردهی به ربات
-app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token("TOKEN").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_link))
 app.add_handler(CallbackQueryHandler(button_handler))
